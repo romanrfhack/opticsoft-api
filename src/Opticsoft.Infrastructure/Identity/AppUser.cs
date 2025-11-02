7c2286b130e4fc382942c4b26fe96aba5440b8d6
romanrfhack <romanrfhack@gmail.com>
2025-11-01 20:42:27 -0600

    Verison del API con multitenant

---
+++ src/Opticsoft.Infrastructure/Identity/AppUser.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+
+namespace Opticsoft.Infrastructure.Identity;
+
+public class AppUser : IdentityUser<Guid>
+{
+    public string? FullName { get; set; }
+    public Guid SucursalId { get; set; }
+    public Guid TenantId { get; set; }
+}
